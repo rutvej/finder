36a53081f9c3894d2b2cf801965a13e29ff34f13
abc <rutvej.waghela@routemobile.com>
2022-07-25 12:34:10 +0530

    Adding Cors

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import os
 app = Flask("__main__")
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLPATH")
 db = SQLAlchemy(app)
